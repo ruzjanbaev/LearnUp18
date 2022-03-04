5d930d8ca8f1892737fdf815d8d1799935e8b2a9
ruzjanbaev <ruzjanbaev@gmail.com>
2022-03-04 11:17:17 +0300

    Update srv config

---
+++ src/main/java/ru/learnup18/aviasales/config/PremiereConfig.java
@@ -0,0 +1,18 @@
+package ru.learnup18.aviasales.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.learnup18.aviasales.model.Premiere;
+import java.util.HashMap;
+
+@Configuration
+public class PremiereConfig {
+
+    //@Primary
+    @Bean
+    public HashMap<String, Premiere> premiereMap() {
+        HashMap<String, Premiere> premiereMap = new HashMap<>();
+        return premiereMap;
+    }
+
+}
--- src/main/java/ru/learnup18/aviasales/services/PremiereService.java
+++ src/main/java/ru/learnup18/aviasales/services/PremiereService.java
@@ -1,5 +1,9 @@
 package ru.learnup18.aviasales.services;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 import ru.learnup18.aviasales.model.Premiere;
 import ru.learnup18.aviasales.services.interfaces.Logger;
@@ -8,31 +12,29 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 @Service
-public class PremiereService {
+public class PremiereService implements ApplicationContextAware {
 
     private Logger logger;
-    static private HashMap<String, Premiere> premiereMap =  new HashMap<String, Premiere>();
+    private ApplicationContext ctx;
+    private HashMap<String, Premiere> premiereMap;
 
-    //@Autowired
-    public PremiereService(Logger logger) {
+    @Autowired
+    public PremiereService(Logger logger, HashMap premiereMap) {
         this.logger = logger;
+        this.premiereMap = premiereMap;
     }
 
     //Добавление
-    public Premiere insPremiere(String name, String description, String ageCategory, Integer countFreeSeats) {
-        logger.print("insPremiere: " + name);
-        Premiere premiere = new Premiere(name, description, ageCategory, countFreeSeats, countFreeSeats);
-        premiereMap.put(name, premiere);
-        return premiere;
+    public void insPremiere(Premiere premiere) {
+        logger.print("insPremiere: " + premiere.getName());
+        premiereMap.put(premiere.getName(), premiere);
     }
 
     //Удаление
-    public boolean delPremiere(String name) {
+    public void delPremiere(String name) {
         logger.print("delPremiere name: " + name);
-        TicketService ticketService = new TicketService(logger);
-        ticketService.delTicketPremiere(name);
+        ctx.getBean(TicketService.class).delTicketPremiere(name);
         premiereMap.remove(name);
-        return true;
     }
 
     //Обновление - Предпологаем что имя Премьеры, это уникальное имя
@@ -54,29 +56,31 @@ public class PremiereService {
         } else {
             logger.print("freeSeats: " + freeSeats + ", freeReal: " + freeReal + ", (freeReal + incTicket): " + (freeReal + incTicket));
             premiereMap.get(name).setCountFreeReal(freeReal + incTicket);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean allToStringPremiere() {
-        for (Premiere s : premiereMap.values()) {
-            oneToStringPremiere(s.getName(), false);
         }
         return true;
     }
 
+    public void allToStringPremiere() {
+        for (Premiere s : premiereMap.values()) {
+            oneToStringPremiere(s.getName(), false);
+        }
+    }
+
     public boolean oneToStringPremiere(String name, boolean ticket) {
-        //System.out.println("premiereMap: " + premiereMap.toString());
         System.out.println("--> Премьера(Событие) : " + premiereMap.get(name).getName() +
                 ", описание : " + premiereMap.get(name).getDescription() +
                 ", возрастная категория : " + premiereMap.get(name).getAgeCategory() +
                 ", кол-о дост-х мест : " + premiereMap.get(name).getCountFreeReal() +
                 ", изначальное кол-о дост-х мест : " + premiereMap.get(name).getCountFreeSeats());
         if (ticket) {
-            TicketService ticketService = new TicketService(logger);
-            ticketService.allToStringTicket(name);
+            ctx.getBean(TicketService.class).allToStringTicket(name);
         }
         return true;
     }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.ctx = ctx;
+    }
+
 }
